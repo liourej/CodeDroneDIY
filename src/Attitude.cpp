1d4ca29176b3730f85672e65ff849172f273c753
liourej <julien.lioure@gmail.com>
2018-05-15 18:01:00 +0200

    WIP

--- src/Attitude.cpp
+++ src/Attitude.cpp
@@ -2,6 +2,7 @@
 #include "Attitude.h"
 #include "CheckIMU.h"
 
+#define AXIS_NB 3
 #define RAD2DEG(angle) angle * 180 / PI
 
 void Attitude::Init()
@@ -28,62 +29,104 @@ void Attitude::Init()
 inline void Attitude::GetCorrectedAccelGyro(float _accMeasures[],
                                             float _gyroMeasures[])
 {
-    int16_t ax, ay, az, gx, gy, gz = 0;
+    int16_t accel[AXIS_NB] = {0, 0, 0};
+    int16_t speed[AXIS_NB] = {0, 0, 0};
 
-    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+    accelgyro.getMotion6(&accel[0], &accel[1], &accel[2], &speed[0], &speed[1], &speed[2]);
 
     // Correct raw data with offset
-    _accMeasures[0] = static_cast<float>(ax - offset[0]) / AcceleroSensitivity;
-    _accMeasures[1] = static_cast<float>(ay - offset[1]) / AcceleroSensitivity;
-    _accMeasures[2] = static_cast<float>(az - offset[2]) / AcceleroSensitivity;
-    _gyroMeasures[0] = static_cast<float>(gx - offset[3]) / GyroSensitivity;
-    _gyroMeasures[1] = static_cast<float>(gy - offset[4]) / GyroSensitivity;
-    _gyroMeasures[2] = static_cast<float>(gz - offset[5]) / GyroSensitivity;
+    for(int axis = 0; axis < AXIS_NB; axis++) {
+      _accMeasures[axis] = static_cast<float>(accel[axis] - offset[axis]) / AcceleroSensitivity;
+      _gyroMeasures[axis] = static_cast<float>(speed[axis] - offset[axis]) / GyroSensitivity;
+    }
 }
 
-// Compute accelerometer and gyroscope offsets
-void Attitude::ComputeOffsets()
-{
-    int16_t accGyroRaw[6] = {0, 0, 0, 0, 0, 0};
-    int32_t offsetSum[6] = {0, 0, 0, 0, 0, 0};
+void ComputeGyroOffsets() {
+    int16_t gyroRaw[AXIS_NB] = {0, 0, 0};
+    int32_t offsetSum[AXIS_NB] = {0, 0, 0};
 
-    int16_t maxVal[6] = {-32768, -32768, -32768, -32768, -32768, -32768};
-    int16_t minVal[6] = {32767, 32767, 32767, 32767, 32767, 32767};
-    int16_t delta[6] = {0, 0, 0, 0, 0, 0};
+    int16_t maxVal[AXIS_NB] = {-32768, -32768, -32768};
+    int16_t minVal[AXIS_NB] = {32767, 32767, 32767};
+    int16_t delta[AXIS_NB] = {0, 0, 0};
 
-    for (int i = 0; i < 6; i++)
-        offset[i] = 0;
+    for (int axis = 0; axis < AXIS_NB; axis++)
+        offset[axis] = 0;
 
     // mean on 10 samples during 2 sec
     for (int sample = 0; sample < 10; sample++) {
-        accelgyro.getMotion6(&accGyroRaw[0], &accGyroRaw[1], &accGyroRaw[2],
-                             &accGyroRaw[3], &accGyroRaw[4], &accGyroRaw[5]);
-        for (int coord = 0; coord < 6; coord++) {
-            if (accGyroRaw[coord] > maxVal[coord])
-                maxVal[coord] = accGyroRaw[coord];
-            if (accGyroRaw[coord] < minVal[coord])
-                minVal[coord] = accGyroRaw[coord];
-            offsetSum[coord] = offsetSum[coord] + accGyroRaw[coord];
+        accelgyro.getRotation(&gyroRaw[0], &gyroRaw[1], &gyroRaw[2]);
+        for (int axis = 0; axis < AXIS_NB; axis++) {
+            if (gyroRaw[axis] > maxVal[axis])
+                maxVal[axis] = gyroRaw[axis];
+            if (gyroRaw[axis] < minVal[axis])
+                minVal[axis] = gyroRaw[axis];
+            offsetSum[axis] = offsetSum[axis] + gyroRaw[axis];
         }
         wdt_reset();
         delay(200);
     }
 
     // Compute mean and delta max
-    for (int i = 0; i < 6; i++) {
-        offset[i] = offsetSum[i] / 10;
-        delta[i] = maxVal[i] - minVal[i];
+    for (int axis = 0; axis < AXIS_NB; axis++) {
+        offset[axis] = offsetSum[axis] / 10;
+        delta[axis] = maxVal[axis] - minVal[axis];
     }
 
     // Check deltas to be sure IMU does not move during mean computation
-    offsetComputed = true;
-    for (int acc = 0; acc < 3; acc++) {
-        if (delta[acc] > (0.2 * AcceleroSensitivity))
+    for (int axis = 0; axis < AXIS_NB; axis++) {
+        if (delta[axis] > (10 * GyroSensitivity)) // 10°/s max
             offsetComputed = false;
     }
 
-    for (int gyro = 3; gyro < 6; gyro++) {
-        if (delta[gyro] > (10 * GyroSensitivity)) // 10°/s max
+    if (offsetComputed) {
+        Serial.print(F("Gyroscope offsets Computed :"));
+        for (int axis = 0; axis < AXIS_NB; axis++) {
+            Serial.print(offset[axis] / GyroSensitivity);
+            Serial.print(" ");
+        }
+        Serial.print("(deg.s-1) ");
+        Serial.print("\n");
+        return true;
+    } else {
+      Serial.println(F("ERROR DURING GYRO OFFSETS COMPUTATION !!"));
+      return false;
+    }
+}
+
+bool Attitude::ComputeAccelOffset() {
+    int16_t accRaw[AXIS_NB] = {0, 0, 0};
+    int32_t offsetSum[AXIS_NB] = {0, 0, 0};
+
+    int16_t maxVal[AXIS_NB] = {-32768, -32768, -32768};
+    int16_t minVal[AXIS_NB] = {32767, 32767, 32767};
+    int16_t delta[AXIS_NB] = {0, 0, 0};
+
+    for (int axis = 0; axis < AXIS_NB; axis++)
+        offset[axis] = 0;
+
+    // mean on 10 samples during 2 sec
+    for (int sample = 0; sample < 10; sample++) {
+        accelgyro.getAcceleration(&accRaw[0], &accRaw[1], &accRaw[2]);
+        for (int coord = 0; coord < 6; coord++) {
+            if (accRaw[coord] > maxVal[coord])
+                maxVal[coord] = accRaw[coord];
+            if (accRaw[coord] < minVal[coord])
+                minVal[coord] = accRaw[coord];
+            offsetSum[coord] = offsetSum[coord] + accRaw[coord];
+        }
+        wdt_reset();
+        delay(200);
+    }
+
+    // Compute mean and delta max
+    for (int axis = 0; axis < AXIS_NB; axis++) {
+        offset[axis] = offsetSum[axis] / 10;
+        delta[axis] = maxVal[axis] - minVal[axis];
+    }
+
+    // Check deltas to be sure IMU does not move during mean computation
+    for (int axis = 0; axis < AXIS_NB; axis++) {
+        if (delta[axis] > (0.2 * AcceleroSensitivity))
             offsetComputed = false;
     }
 
@@ -91,21 +134,24 @@ void Attitude::ComputeOffsets()
     offset[2] = offset[2] - AcceleroSensitivity;
 
     if (offsetComputed) {
-        Serial.print(F("Offsets Computed :"));
-        for (int i = 0; i < 3; i++) {
-            Serial.print(offset[i] / AcceleroSensitivity);
+        Serial.print(F("Acceleration offsets Computed :"));
+        for (int axis = 0; axis < AXIS_NB; axis++) {
+            Serial.print(offset[axis] / AcceleroSensitivity);
             Serial.print(" ");
         }
         Serial.print("(m.s-2) ");
-        for (int i = 3; i < 6; i++) {
-            Serial.print(offset[i] / GyroSensitivity);
-            Serial.print(" ");
-        }
-        Serial.print("(deg.s-1) ");
-        Serial.print("\n");
+        return true;
     } else {
-        Serial.println(F("ERROR DURING OFFSETS COMPUTATION !!"));
+      Serial.println(F("ERROR DURING ACCELERATION OFFSETS COMPUTATION !!"));
+      return false;
     }
+}
+
+// Compute accelerometer and gyroscope offsets
+void Attitude::ComputeOffsets()
+{
+ if ( ComputeGyroOffsets() && ComputeAccelOffset())
+   offsetComputed = true;
 }
 
 inline void Attitude::Normalize(float _acc[])
@@ -127,8 +173,8 @@ float Attitude::GetFilterTimeConstant(float _loopTimeSec)
 void Attitude::GetCurrPos(float _pos[], float _speed[], float _loop_time)
 {
     // float roll, pitch = 0;
-    float accRaw[3] = {0, 0, 0};
-    float gyroRaw[3] = {0, 0, 0};
+    float accRaw[AXIS_NB] = {0, 0, 0};
+    float gyroRaw[AXIS_NB] = {0, 0, 0};
 
     // Get corrected data from gyro and accelero
     GetCorrectedAccelGyro(accRaw, gyroRaw);
